eea4490746af3f290217821a9602ea5a039aef1f
kmner <56357254+kmner@users.noreply.github.com>
2022-09-03 13:16:36 +0800

    change ui
    change ui

--- main.py
+++ main.py
@@ -102,10 +102,11 @@ class yszfplugin(StellarPlayer.IStellarPlayerPlugin):
                 'width':1.0,
                 'height':30
             },
-            {'type':'space','height':10},
-            {'type':'grid','name':'zygrid','itemlayout':zywz_layout,'value':self.spy,'itemheight':30,'itemwidth':80,'height':70},
             {'type':'space','height':5},
-            {'type':'grid','name':'mediaclassgrid','itemlayout':mediaclass_layout,'value':self.mediaclass,'itemheight':30,'itemwidth':80,'height':80},
+            'group': [
+                {'type':'grid','name':'zygrid','itemlayout':zywz_layout,'value':self.spy,'itemheight':30,'itemwidth':80,'height':150},
+                {'type':'grid','name':'mediaclassgrid','itemlayout':mediaclass_layout,'value':self.mediaclass,'itemheight':30,'itemwidth':80,'height':150},
+            ]
             {'type':'space','height':5},
             {'type':'grid','name':'mediagrid','itemlayout':mediagrid_layout,'value':self.medias,'separator':True,'itemheight':240,'itemwidth':150},
             {'group':
